a3342d6019a2ba552ed356403415c2fd96adc60d
k8pai <thek8pai@gmail.com>
2023-04-20 23:57:46 +0530

    updated TiFiles.js to a much stable and compact version

--- src/components/TiFiles.js
+++ src/components/TiFiles.js
@@ -1,77 +1,43 @@
 import React, { Fragment, useContext, useEffect, useState } from 'react';
 import { TiFileContext, TiFormContext } from '../lib/Context';
 
-const TiLabel = ({ name, title, ...rest }) => {
-	return (
-		<label htmlFor={name} {...rest}>
-			{title}
-		</label>
-	);
-};
-
-const TiFileWrapper = ({ className, label = 'Choose a File', children }) => {
-	const { name } = useContext(TiFileContext);
-	return (
-		<label htmlFor={`${name}-button`} className={className}>
-			<span className="h-full w-full whitespace-nowrap">{label}</span>
-			{children}
-		</label>
-	);
-};
-
-const TiFile = ({ className, ...rest }) => {
-	const { name, setIsDisabled, setFile } = useContext(TiFileContext);
-
-	useEffect(() => {
-		return () => {
-			if (rest.disabled) {
-				setIsDisabled(true);
-			}
-		};
-	}, []);
-
-	const handleFileChange = (event) => {
-		const files = event.target.files[0];
-		setFile(files);
-	};
-
-	return (
-		<input
-			id={`${name}-button`}
-			name={`${name}-button`}
-			type="file"
-			className={`sr-only ${className}`}
-			onChange={handleFileChange}
-			{...rest}
-		/>
-	);
-};
-
-const TiInfo = ({ fallback = 'No file selected' }) => {
-	const { file } = useContext(TiFileContext);
-
-	return (
-		<Fragment>
-			{file ? (
-				<p className="text-sm text-gray-500">
-					{file.name} | ({file.size} bytes)
-				</p>
-			) : (
-				<p className="text-sm text-gray-500">{fallback}</p>
-			)}
-		</Fragment>
-	);
-};
-
-TiFiles.Label = TiLabel;
-TiFiles.FileWrapper = TiFileWrapper;
-TiFiles.File = TiFile;
-TiFiles.Info = TiInfo;
-
-export default function TiFiles({ name, children, className }) {
+export default function TiFiles({
+	name,
+	title,
+	label = 'Choose a File',
+	fallback,
+	className = '',
+	style = {},
+	...rest
+}) {
 	const { setValues } = useContext(TiFormContext);
 	const [file, setFile] = useState(null);
 	const [disabled, setDisabled] = useState(false);
+	const [theme, setTheme] = useState({
+		border: 'border-indigo-600',
+		bg: 'bg-white',
+		heading: 'text-indigo-600',
+		text: 'text-gray-500',
+		...style,
+	});
+
+	useEffect(() => {
+		return () =>
+			setValues == '()=>{}'
+				? console.warn(
+						`You need to enclose <TiFiles {...props} /> within <TiForm>, to access values of ${name} in your TiForm's submitHandler function.`,
+				  )
+				: null;
+	}, []);
+
+	useEffect(() => {
+		console.log('theme = ', theme);
+		return () => {
+			if (rest.disabled) {
+				setDisabled(true);
+			}
+		};
+	}, []);
 
 	useEffect(() => {
 		if (setValues) {
@@ -79,8 +45,13 @@ export default function TiFiles({ name, children, className }) {
 		}
 	}, [file]);
 
+	const handleFileChange = (event) => {
+		const files = event.target.files[0];
+		setFile(files);
+	};
+
 	return (
-		<div className={className}>
+		<div className={`px-3 py-2 ${className}`}>
 			<TiFileContext.Provider
 				value={{
 					name,
@@ -90,7 +61,43 @@ export default function TiFiles({ name, children, className }) {
 					setFile,
 				}}
 			>
-				{children({ isDisabled: disabled })}
+				<label htmlFor={name}>{title}</label>
+
+				<div
+					className={`flex items-center border-2 ${theme.border} rounded-lg`}
+				>
+					<label
+						htmlFor={`${name}-button`}
+						className={`m-2 p-2 py-1 shadow rounded-md border font-medium cursor-pointer ${theme.bg} ${theme.border}`}
+					>
+						<span
+							className={`h-full w-full whitespace-nowrap ${theme.heading}`}
+						>
+							{label}
+						</span>
+						<input
+							id={`${name}-button`}
+							name={`${name}-button`}
+							type="file"
+							className={`sr-only`}
+							onChange={handleFileChange}
+							{...rest}
+						/>
+					</label>
+					<div className="ml-3 mr-5">
+						<Fragment>
+							{file ? (
+								<p className={`text-sm ${theme.text}`}>
+									{file.name} | ({file.size} bytes)
+								</p>
+							) : (
+								<p className={`text-sm ${theme.text}`}>
+									{fallback}
+								</p>
+							)}
+						</Fragment>
+					</div>
+				</div>
 			</TiFileContext.Provider>
 		</div>
 	);
