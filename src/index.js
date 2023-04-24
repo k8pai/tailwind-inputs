08c3cce7b51b27f218141b664d400775d45c5d01
k8pai <thek8pai@gmail.com>
2023-04-24 23:02:05 +0530

    updated with import statements

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 // index.js
-
+export { TiButton } from './components/TiButton';
 export { TiForm } from './components/TiForm';
 export { TiLabel } from './components/TiLabel';
-export { TiButton } from './components/TiButton';
-export { TiRange } from './components/TiRange';
 export { TiRadio } from './components/TiRadio';
-export { TiMail } from './components/TiMail';
-export { TiText } from './components/TiText';
-export { TiNumber } from './components/TiNumber';
-export { default as TiMultiselect } from './components/TiMultiSelect';
-export { default as TiFiles } from './components/TiFiles';
-export { default as TiSelect } from './components/TiSelect';
+export { TiRange } from './components/TiRange';
+
 export { default as TiCheckbox } from './components/TiCheckbox';
+export { default as TiCombobox } from './components/TiCombobox';
+export { default as TiDisclosure } from './components/TiDisclosure';
+export { default as TiFiles } from './components/TiFiles';
+export { default as TiFloatingLabel } from './components/TiFloatingLabel';
+export { default as TiMail } from './components/TiMail';
+export { default as TiModal } from './components/TiModal';
+export { default as TiMultiselect } from './components/TiMultiSelect';
+export { default as TiNumber } from './components/TiNumber';
+export { default as TiPassword } from './components/TiPassword';
+export { default as TiSearch } from './components/TiSearch';
+export { default as TiSearchWithTag } from './components/TiSearchWithTag';
+export { default as TiSelect } from './components/TiSelect';
+export { default as TiText } from './components/TiText';
+export { default as TiTextarea } from './components/TiTextarea';
+export { default as TiToggle } from './components/TiToggle';
