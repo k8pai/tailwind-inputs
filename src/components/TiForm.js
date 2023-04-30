daeb19260b9c2b8db34e161c260f3cb00b77a0b4
k8pai <thek8pai@gmail.com>
2023-04-30 17:24:56 +0530

    updated with action and method

--- src/components/TiForm.js
+++ src/components/TiForm.js
@@ -1,19 +1,34 @@
 import React, { useState } from 'react';
 import { TiFormContext } from '../lib/Context';
 
-export function TiForm({ initialValues = {}, className, onSubmit, children }) {
+export function TiForm({
+	initialValues = {},
+	className,
+	onSubmit = () => {},
+	children,
+	...rest
+}) {
 	const [values, setValues] = useState(initialValues);
 	const [submit, setSubmit] = useState(false);
-	const [valid, setValid] = useState({});
+	const [valid, setValid] = useState([]);
 
-	const handleSubmit = (event) => {
+	const handleSubmit = async (event) => {
 		event.preventDefault();
+		if (rest.action && rest.method) {
+			await fetch(rest.action, {
+				method: rest.method,
+				body: JSON.stringify(values),
+			});
+		}
 		setSubmit(true);
 		onSubmit(values);
+		setTimeout(() => {
+			setSubmit(false);
+		}, 2000);
 	};
 
 	return (
-		<form onSubmit={handleSubmit} className={className}>
+		<form onSubmit={handleSubmit} className={className} {...rest}>
 			<TiFormContext.Provider
 				value={{
 					values,
